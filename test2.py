89dc7f0eb990baf6c29613690d7d79a43989c76b
Owzok <martin.perezbonany@utec.edu.pe>
2023-05-29 02:25:31 -0500

    ya tiene gui, me olvide de borrar los tests xd

--- test2.py
+++
@@ -1,5 +0,0 @@
-file_paths = ['./blocks/block-0.txt', './blocks/block-1.txt', './blocks/block-2.txt', './blocks/block-3.txt']
-
-merged_block = merge_all_blocks(file_paths)
-
-write_to_disk_with_tfidf(merged_block, "spimi_inverted_index_good.txt", 6)
